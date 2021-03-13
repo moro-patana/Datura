91539e5f46fb9ebee03a8d6cd0b13a057f5b08b9
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-03-13 10:53:05 +0300

    set the nav menu

---
+++ src/components/NavMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function NavMenu() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/about">About</a></li>
+                <li><a href="/portfolio">Portfolio</a></li>
+                <li><a href="/blogs"></a>Blogs</li>
+                <li><a href="/contacts">Contacts</a></li>
+            </ul>
+        </nav>
+    )
+}
