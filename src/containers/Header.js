7928cb97f32c1b8b7f0714c858b7517ecc95ed08
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-03-13 10:59:41 +0300

    created a state to toggle the menu

---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavMenu from "../components/NavMenu"
+
+export default function Header() {
+    const [showMenu, setShowMenu] = useState(false)
+
+    function toggleMenu() {
+        setShowMenu(!showMenu)
+    }
+    return (
+        <header>
+            <div>
+                <h1>Datura</h1>
+                <button onClick={toggleMenu}>Open</button>
+                <NavMenu/>
+            </div>
+        </header>
+    )
+}
